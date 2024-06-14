33da25aa148c8cdb50027aa70f8635143851b551
AntAresWeb <andre.davy777@gmail.com>
2024-06-14 10:01:21 +0300

    2024-06-14 (2)

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 python3.11 -m venv venv
-source venv/bin/activate
+source ./venv/bin/activate
 python3.11 -m pip install --upgrade pip
 pip install -r requirements.txt
 python3 ./productshop/manage.py makemigrations
